f158e71a638391b207854b63b4162d4c5d1bf7ee
Yukta Dhungana <yuktadhungana8@gmail.com>
2026-03-07 08:47:59 +0545

    fix gmail smtp port for railway

--- lifelink/settings.py
+++ lifelink/settings.py
@@ -199,8 +199,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
+
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = 'LifeLink Nepal <noreply@lifelink.com>'
