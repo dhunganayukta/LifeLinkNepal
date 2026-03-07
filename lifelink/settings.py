754440ffdc12abfeed20cc5588e40fa5b88c0ba6
Yukta Dhungana <yuktadhungana8@gmail.com>
2026-03-07 08:55:18 +0545

    switch to resend smtp

--- lifelink/settings.py
+++ lifelink/settings.py
@@ -198,16 +198,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ========================
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-
-
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST = 'smtp.resend.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'resend'
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = 'LifeLink Nepal <noreply@lifelink.com>'
+DEFAULT_FROM_EMAIL = 'LifeLink Nepal <onboarding@resend.dev>'
 
 # ========================
 # CELERY (Redis from Railway)
